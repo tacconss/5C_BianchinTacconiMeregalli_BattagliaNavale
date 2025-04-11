bfdbe81d720abb68a2bbfc0162f6136442bbb504
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2025-04-11 11:43:50 +0200

    colpo

---
+++ src/components/colpo.js
@@ -0,0 +1,5 @@
+export const generateTurno = (parentElement) => {
+    let coordinatebersaglio; /*istanza di coordinate*/
+    let risultato=false/*se il colpo è andato a segno  true sennò false*/
+    let giocatoreSparante/*istanza di giocatore*/;
+};
