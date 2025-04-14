f0c3027475504f47c874150820b3752ab3003e2c
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-04-14 09:01:25 +0200

    home.js per la pagina home

---
+++ src/components/home.js
@@ -0,0 +1,19 @@
+
+
+
+export function updatePlayerList(users) {
+  let html = "";
+  for (let i = 0; i < users.length; i++) {
+    html += "<li>" + users[i].name + "</li>";
+  }
+  document.getElementById("player-list").innerHTML = html;
+}
+
+
+export function updateGameList(games = []) {
+  let html = "";
+  for (let i = 0; i < games.length; i++) {
+    html += "<li>🏠 " + games[i] + "</li>";
+  }
+  document.getElementById("game-list").innerHTML = html;
+}
