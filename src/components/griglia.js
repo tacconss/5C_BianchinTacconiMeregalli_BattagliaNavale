d8e195013a2212d82371f8009d46189ada4a3e4b
Emanuele Meregalli <c.meregalliemanuele@itis-molinari.eu>
2025-04-14 11:35:22 +0200

    correzione server.js

--- src/components/griglia.js
+++ src/components/griglia.js
@@ -1,6 +1,6 @@
 export const generateGridComponent = (parentElement) => {
     let dimensione;
-    let casella=[/*istanza di coordinate x*/][/*istanza di coordinate y*/]; /*array di coordinate*/
+   // let casella=[/*istanza di coordinate x*/][/*istanza di coordinate y*/]; /*array di coordinate*/
     let tutteNaviAffondate=false; /*se tutte le navi sono affondate è true sennò false*/
     return {
         posizionaNave: (/*istanza di nave, istanza di coordinate*/) => {
