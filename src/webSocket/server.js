06d4881afe7e1eeb0d1f53eebf9d812c4fb5637d
Davide Bianchin <bianchindavide@itis-molinari.eu>
2025-04-14 09:03:19 +0200

    server della websocket

---
+++ src/webSocket/server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "..")));
+
+server.listen(PORT, () => {
+  console.log(`Server attivo su http://localhost:${PORT}/pages/home.html`);
+});
+
+let users = [];
+
+io.on("connection", (socket) => {
+  console.log("Connesso:", socket.id);
+
+  socket.on("join", (username) => {
+    if (!username || users.find(u => u.name === username)) {
+      socket.emit("join_error", "Nome già in uso o non valido.");
+      return;
+    }
+
+    users.push({ name: username, socketId: socket.id, playing: false });
+    io.emit("list", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnesso:", socket.id);
+    users = users.filter(u => u.socketId !== socket.id);
+    io.emit("list", users);
+  });
+});
